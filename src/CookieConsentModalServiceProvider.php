0f9be654ba6198e4a43660189ca11ac0939cfc0e
Martin Schenk <mschenk.pda@gmail.com>
2023-01-24 21:03:06 +0100

    Update CookieConsentModalServiceProvider.php
    removed app->register

--- src/CookieConsentModalServiceProvider.php
+++ src/CookieConsentModalServiceProvider.php
@@ -13,8 +13,6 @@ class CookieConsentModalServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->register('Martinschenk\LivewireCookieConsent\CookieConsentModalServiceProvider');
-        
         $this->mergeConfigFrom(__DIR__.'/../config/livewire-cookie-consent.php', 'livewire-cookie-consent');
     }
 
